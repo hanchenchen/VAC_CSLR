770feb7c9ce901da334c35a32e94dcdc8b33739c
Han Chenchen <13980209828@163.com>
2022-09-13 19:29:23 +0800

    Train 00-baseline

---
+++ test.sh
@@ -0,0 +1,5 @@
+python main.py  \
+--config configs/baseline.yaml  \
+--load-weights checkpoints/resnet18_vac_smkd_dev_19.80_epoch35_model.pt \
+--phase test \
+--work-dir work-dir/test/resnet18_vac_smkd_dev_19.80_epoch35_model/
---
+++ train.sh
@@ -0,0 +1,4 @@
+python main.py  \
+--config configs/baseline.yaml  \
+--device 0,1 \
+--work-dir work-dir/train/00-baseline/
