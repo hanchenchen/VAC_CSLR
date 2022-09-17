ba2200437977e4eeb0a94730848f19430cb5c7c6
hanchenchen <13980209828@163.com>
2022-09-17 20:37:55 +0800

    Debug and resume 05

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 python main.py  \
 --config configs/05-02-fpn.yaml  \
 --device 4,5 \
---work-dir work-dir/train/05-02-fpn/
+--work-dir work-dir/train/05-02-fpn/ \
+--load-checkpoints work-dir/train/05-02-fpn/dev_23.40_epoch14_model.pt
