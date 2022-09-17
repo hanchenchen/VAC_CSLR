504721c60fc8ac0acec21aa82905891af1412d33
hanchenchen <13980209828@163.com>
2022-09-17 14:32:37 +0800

    Update

--- main.py
+++ main.py
@@ -55,6 +55,9 @@ class Processor():
                 if save_model:
                     model_path = "{}dev_{:05.2f}_epoch{}_model.pt".format(self.arg.work_dir, dev_wer, epoch)
                     seq_model_list.append(model_path)
+                    model_path = sorted(model_path)
+                    for path in model_path[3:]:
+                        os.remove(path)
                     print("seq_model_list", seq_model_list)
                     self.save_model(epoch, model_path)
         elif self.arg.phase == 'test':
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python main.py  \
---config configs/baseline+vac+stmc.yaml  \
---device 2,3 \
---work-dir work-dir/train/02-baseline+vac+stmc/
+--config configs/05-02-fpn.yaml  \
+--device 4,5 \
+--work-dir work-dir/train/05-02-fpn/
