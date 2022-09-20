ca68e636438d57c0ba18bf58f1fcbbd0fd760ffb
Han Chenchen <13980209828@163.com>
2022-09-20 11:11:49 +0800

    12-lr=4e-6-12-layer-tf-02

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python main.py  \
---config configs/08-add-clip-weight-00.yaml  \
+--config configs/baseline+vac+stmc.yaml  \
 --device 0,1 \
---work-dir work-dir/train/09-12-layer-tf-08/
+--work-dir work-dir/train/12-lr=4e-6-12-layer-tf-02/
