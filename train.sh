348a02fdf685bc1b1a3eade375f761d8f9030e33
Han Chenchen <13980209828@163.com>
2022-09-20 11:21:20 +0800

    13-4-gpu-12-lr=4e-6-12-layer-tf-02

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 python main.py  \
 --config configs/baseline+vac+stmc.yaml  \
---device 0,1 \
---work-dir work-dir/train/12-lr=4e-6-12-layer-tf-02/
+--device 0,1,6,7 \
+--batch-size 4 \
+--work-dir work-dir/train/13-4-gpu-12-lr=4e-6-12-layer-tf-02/
