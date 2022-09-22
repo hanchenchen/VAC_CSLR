d202f17260d0b33a5a189331b5a0f81daf8b1e2c
Han Chenchen <13980209828@163.com>
2022-09-22 23:18:27 +0800

    20-dist-bs=2-02

--- dist_train.sh
+++ dist_train.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0,1,6,7 python -m torch.distributed.launch \
---nproc_per_node=4 --master_port=4444 main.py  \
+CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch \
+--nproc_per_node=2 --master_port=4253 main.py  \
 --config configs/baseline+vac+smkd.yaml  \
---device 0,1,2,3 \
---batch-size 4 \
---work-dir work-dir/train/19-lr=5e-4-18/
+--device 0,1 \
+--batch-size 1 \
+--work-dir work-dir/train/20-dist-bs=2-02/
