31bd01b4f20b3025cc21d52b0414c80ef757ea47
Han Chenchen <13980209828@163.com>
2022-09-25 14:19:29 +0800

    03-lr=2e-4-02-baseline+vac+smkd

--- dist_train.sh
+++ dist_train.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0,1,4,5 python -m torch.distributed.launch \
 --config configs/baseline+vac+smkd.yaml  \
 --device 0,1,2,3 \
 --batch-size 4 \
---work-dir work-dir/dist-gpu=4-bs=4-train/02-baseline+vac+smkd/ \
+--work-dir work-dir/dist-gpu=4-bs=4-train/03-lr=2e-4-02-baseline+vac+smkd/ \
 # --load-weights checkpoints/resnet18_vac_smkd_dev_19.80_epoch35_model.pt \
 # --phase test \
 # --work-dir work-dir/test/22-backward_hook-21-dist-bs=2-gpu=1-02/
