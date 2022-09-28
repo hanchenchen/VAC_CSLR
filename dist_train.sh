2cc83179fcabbb1b64500f440b27e61fb9ec9ffe
Han Chenchen <13980209828@163.com>
2022-09-28 12:17:40 +0800

    19-debug-flip-18-13-11-09

--- dist_train.sh
+++ dist_train.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch \
---nproc_per_node=2 --master_port=3246 main.py  \
+CUDA_VISIBLE_DEVICES=4,5 python -m torch.distributed.launch \
+--nproc_per_node=2 --master_port=3247 main.py  \
 --config configs/18-dtw-bidire-13-11-09.yaml  \
 --device 0,1 \
 --batch-size 1 \
---work-dir work-dir/dist-gpu=2xbs=1-train/18-dtw-bidire-13-11-09/ \
+--work-dir work-dir/dist-gpu=2xbs=1-train/19-debug-flip-18-13-11-09/ \
 # --load-weights checkpoints/resnet18_vac_smkd_dev_19.80_epoch35_model.pt \
 # --phase test \
 # --work-dir work-dir/test/22-backward_hook-21-dist-bs=2-gpu=1-02/
