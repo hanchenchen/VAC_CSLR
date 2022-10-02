a78e09030edc6bb6dcb99b29731fb044b8a958d8
Han Chenchen <13980209828@163.com>
2022-10-02 12:56:18 +0800

    33-bs=1-32

--- dist_train.sh
+++ dist_train.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch \
---nproc_per_node=2 --master_port=3444 main.py  \
+CUDA_VISIBLE_DEVICES=4,5 python -m torch.distributed.launch \
+--nproc_per_node=2 --master_port=3445 main.py  \
 --config configs/27-ctc-reg-decoder-26.yaml  \
 --device 0,1 \
---batch-size 2 \
---work-dir work-dir/dist-gpu=2xbs=1-train/32-simsiam-head-align-2-level-features-29/ \
+--batch-size 1 \
+--work-dir work-dir/dist-gpu=2xbs=1-train/33-bs=1-32/ \
 # --load-weights checkpoints/resnet18_vac_smkd_dev_19.80_epoch35_model.pt \
 # --phase test \
 # --work-dir work-dir/test/22-backward_hook-21-dist-bs=2-gpu=1-02/
