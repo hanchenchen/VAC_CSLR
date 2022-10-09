af94da99bcc0586f0ebbf53333bbae4c9a5d07e8
hanchenchen <13980209828@163.com>
2022-10-09 10:57:29 +0800

    1009-25-wo-ConvCTC-24

--- dist_train.sh
+++ dist_train.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=6,7 nohup python -m torch.distributed.launch \
+CUDA_VISIBLE_DEVICES=0,1 nohup python -m torch.distributed.launch \
 --nproc_per_node=2 --master_port=3561 main.py  \
 --config configs/1002-02-conv-ctc-00.yaml  \
 --device 0,1 \
 --batch-size 1 \
---work-dir work-dir/dist-gpu=2xbs=1-train/1009-24-weight_norm=False-23/ & \
+--work-dir work-dir/dist-gpu=2xbs=1-train/1009-25-wo-ConvCTC-24/ & \
 # --load-weights checkpoints/resnet18_vac_smkd_dev_19.80_epoch35_model.pt \
 # --phase test \
 # --work-dir work-dir/test/22-backward_hook-21-dist-bs=2-gpu=1-02/
